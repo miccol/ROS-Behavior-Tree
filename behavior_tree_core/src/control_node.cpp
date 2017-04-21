4a1469287d3a606f50667c9da6b68f6e5383eb7e
michele <miccol@kth.se>
2017-04-21 16:57:07 +0200

    control_node.cpp roslint compliant

--- behavior_tree_core/src/control_node.cpp
+++ behavior_tree_core/src/control_node.cpp
@@ -1,11 +1,24 @@
-#include <control_node.h>
+/* Copyright (C) 2015-2017 Michele Colledanchise - All Rights Reserved
+*
+*   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+*   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*   and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+*   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+*   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
+
+#include <control_node.h>
+#include <string>
+#include <vector>
 
 BT::ControlNode::ControlNode(std::string name) : TreeNode::TreeNode(name)
 {
     type_ = BT::CONTROL_NODE;
     ReturnStatus child_i_status_ = BT::IDLE;
-
 }
 
 BT::ControlNode::~ControlNode() {}
@@ -37,19 +50,15 @@ void BT::ControlNode::Halt()
     set_status(BT::HALTED);
 }
 
-
-
 std::vector<BT::TreeNode*> BT::ControlNode::GetChildren()
 {
     return children_nodes_;
 }
 
-
 void BT::ControlNode::ResetColorState()
 {
-
     set_color_status(BT::IDLE);
-    for(unsigned int i = 0; i < children_nodes_.size(); i++)
+    for (unsigned int i = 0; i < children_nodes_.size(); i++)
     {
         children_nodes_[i]->ResetColorState();
     }
@@ -57,31 +66,26 @@ void BT::ControlNode::ResetColorState()
 
 void BT::ControlNode::HaltChildren(int i)
 {
-    for(unsigned int j=i; j<children_nodes_.size(); j++)
+    for (unsigned int j=i; j < children_nodes_.size(); j++)
     {
-        //TODO FIX this
-        //        if (children_nodes_[j]->get_type() != BT::CONDITION_NODE  && children_nodes_[j]->get_status() != BT::IDLE)
-
         if (children_nodes_[j]->get_type() == BT::CONDITION_NODE)
         {
             children_nodes_[i]->ResetColorState();
         }
         else
         {
-
             if (children_nodes_[j]->get_status() == BT::RUNNING)
             {
                 DEBUG_STDOUT("SENDING HALT TO CHILD " << children_nodes_[j]-> get_name());
                 children_nodes_[j]->Halt();
-
             }
             else
             {
-                DEBUG_STDOUT("NO NEED TO HALT " << children_nodes_[j]-> get_name() << "STATUS" << children_nodes_[j]->get_status());
+                DEBUG_STDOUT("NO NEED TO HALT " << children_nodes_[j]-> get_name()
+                             << "STATUS" << children_nodes_[j]->get_status());
             }
         }
     }
-
 }
 
 int BT::ControlNode::Depth()
@@ -95,7 +99,6 @@ int BT::ControlNode::Depth()
         {
             depMax = dep;
         }
-
     }
     return 1 + depMax;
 }
