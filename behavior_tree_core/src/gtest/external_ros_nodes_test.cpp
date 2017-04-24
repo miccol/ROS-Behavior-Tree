2a2f2c017a9d490f0bcb007d1b0de9cf737edf94
michele <miccol@kth.se>
2017-04-24 16:51:08 +0200

    external_ros_nodes_test.cpp roslint compliant

--- behavior_tree_core/src/gtest/external_ros_nodes_test.cpp
+++ behavior_tree_core/src/gtest/external_ros_nodes_test.cpp
@@ -1,5 +1,17 @@
-#include "behavior_tree.h"
+/* Copyright (C) 2015-2017 Michele Colledanchise - All Rights Reserved
+*
+*   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+*   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*   and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+*   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+*   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
+
+#include <behavior_tree.h>
 
 int main(int argc, char **argv)
 {
@@ -14,13 +26,11 @@ int main(int argc, char **argv)
 
 
         BT:: SequenceNode* sequence1 = new BT::SequenceNode("seq1");
-        //SequenceStarNode* sequence1 = new SequenceStarNode("seq1");
 
         sequence1->AddChild(condition);
         sequence1->AddChild(action);
 
-        Execute(sequence1, TickPeriod_milliseconds);//from BehaviorTree.cpp
-
+        Execute(sequence1, TickPeriod_milliseconds);  // from BehaviorTree.cpp
     }
     catch (BT::BehaviorTreeException& Exception)
     {
