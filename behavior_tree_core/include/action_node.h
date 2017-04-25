4e2cac65ba28c715683df7f99127a3ccbe865a33
michele <miccol@kth.se>
2017-04-25 09:57:25 +0200

    action_node.h roslint compliant

--- behavior_tree_core/include/action_node.h
+++ behavior_tree_core/include/action_node.h
@@ -1,8 +1,20 @@
-#ifndef BEHAVIORTREECORE_ACTIONNODE_H
-#define BEHAVIORTREECORE_ACTIONNODE_H
+/* Copyright (C) 2015-2017 Michele Colledanchise - All Rights Reserved
+*
+*   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+*   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*   and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+*   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+*   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef ACTION_NODE_H
+#define ACTION_NODE_H
 
 #include <leaf_node.h>
-
+#include <string>
 namespace BT
 {
 
@@ -10,7 +22,7 @@ class ActionNode : public LeafNode
 {
 public:
     // Constructor
-    ActionNode(std::string name);
+    explicit ActionNode(std::string name);
     ~ActionNode();
 
     // The method that is going to be executed by the thread
@@ -25,6 +37,6 @@ public:
     bool WriteState(ReturnStatus new_state);
     int DrawType();
 };
-}
+}  // namespace BT
 
-#endif
+#endif  // ACTION_NODE_H
